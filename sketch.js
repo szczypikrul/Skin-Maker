ba155615ec5ab2da063bc3da246d8b7815a6d808
szczypikrul <42851381+szczypikrul@users.noreply.github.com>
2024-12-07 21:47:44 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,27 +2,33 @@ let mario;
 let marioUV;
 
 function preload() {
+  // Ładujemy model i teksturę
   mario = loadModel('model/model.obj', true);
   marioUV = loadImage('img/steve.png');
 }
 
 function setup() {
   createCanvas(400, 400, WEBGL);
+
+  // Konfiguracja tekstury dla ostrości
+  noSmooth(); // Wyłącza antyaliasing dla tekstur
+  marioUV.resize(marioUV.width, marioUV.height); // Upewnij się, że tekstura zachowuje pełną rozdzielczość
+  marioUV.loadPixels(); // Wymusza załadowanie pikseli tekstury
 }
 
 function draw() {
-  stroke(0);
-  strokeWeight(0);
   background(100);
 
-  // Dodaj interaktywną kontrolę
+  // Sterowanie kamerą
   orbitControl(5);
 
-  // Obrót modelu (jeśli wymagany)
-  rotateZ(PI);
+  // Konfiguracja oświetlenia
+  ambientLight(255); // Pełne, równomierne oświetlenie
 
-  // Renderuj model z teksturą
-  texture(marioUV); // Tekstura musi być nałożona przed `model()`
-  textureMode(NORMAL);
+  // Renderowanie modelu
+  push();
+  texture(marioUV); // Nakładanie tekstury
+  noStroke(); // Usunięcie obramowania
   model(mario);
+  pop();
 }
