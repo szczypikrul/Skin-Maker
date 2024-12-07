391a77499e14c9c22d31bfaf63e906d231c27c05
szczypikrul <42851381+szczypikrul@users.noreply.github.com>
2024-12-07 22:18:31 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,32 +2,20 @@ let mario;
 let marioUV;
 
 function preload() {
-  // Ładujemy model i teksturę
   mario = loadModel('model/model.obj', true);
   marioUV = loadImage('img/steve.png');
 }
 
 function setup() {
   createCanvas(400, 400, WEBGL);
-
-  // Wyłączenie interpolacji tekstury
-  textureWrap(REPEAT, REPEAT);
-  marioUV.setInterpolation(NEAREST);
 }
 
 function draw() {
+  stroke(0);
+  strokeWeight(0);
   background(100);
-
-  // Sterowanie kamerą
-  orbitControl(5);
-
-  // Oświetlenie
-  ambientLight(255); // Równomierne oświetlenie dla pełnej widoczności
-
-  // Renderowanie modelu z teksturą
-  push();
-  texture(marioUV);
-  noStroke(); // Usunięcie obramowania modelu
+  orbitControl(5)
+  rotateZ(PI)
   model(mario);
-  pop();
+  texture(marioUV);
 }
