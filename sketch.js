2f71b3c73bcf7af743803b8c861bafc0fa67fb10
szczypikrul <42851381+szczypikrul@users.noreply.github.com>
2024-12-08 03:54:49 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,8 +10,13 @@ function preload() {
 function setup() {
   createCanvas(400, 400, WEBGL);
 
-  // Wyłączamy interpolację tekstury
-  marioUV.setInterpolation(NEAREST, NEAREST);
+  // Uzyskaj dostęp do kontekstu WebGL
+  const gl = this._renderer.GL;
+  
+  // Wyłącz interpolację tekstury
+  const tex = canvas.getTexture(marioUV);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 }
 
 function draw() {
