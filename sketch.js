488c8f1d6a80fd0fa33644ca39e5e2f96fcf5754
szczypikrul <42851381+szczypikrul@users.noreply.github.com>
2024-12-08 04:07:30 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let mario;
+/*let mario;
 let marioUV;
 let tex;
 
@@ -27,4 +27,37 @@ function draw() {
   // Renderowanie modelu z teksturą
   texture(marioUV); // Nałóż teksturę na model
   model(mario);     // Wyświetl model
+}*/
+let model3D;
+let texture1, texture2;
+let tex1, tex2;
+
+function preload() {
+  model3D = loadModel('model/model.obj', true);
+  texture1 = loadImage('img/jaskiniowiec_skin.png');
+  texture2 = loadImage('img/steve.png');
 }
+
+function setup() {
+  createCanvas(400, 400, WEBGL);
+
+  // Pobieramy tekstury i ustawiamy interpolację na NEAREST
+  tex1 = createGraphics(texture1.width, texture1.height);
+  tex1.image(texture1, 0, 0);
+  
+  tex2 = createGraphics(texture2.width, texture2.height);
+  tex2.image(texture2, 0, 0);
+  
+  // Możesz połączyć obie tekstury na jednym obiekcie graficznym
+  tex1.blend(tex2, 0, 0, texture2.width, texture2.height, 0, 0, texture1.width, texture1.height, ADD);
+}
+
+function draw() {
+  background(200);
+  orbitControl();
+
+  // Renderujemy model z nałożonymi teksturami
+  texture(tex1); // Łączona tekstura
+  model(model3D);
+}
+
