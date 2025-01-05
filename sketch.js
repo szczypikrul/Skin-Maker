c9fded3e9b2cc15d854b2b6f2be9ac32c231fb05
szczypikrul <42851381+szczypikrul@users.noreply.github.com>
2025-01-05 01:14:18 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,49 +32,44 @@ function draw() {
 
 */
 let mario;
-let combinedTexture;
+let marioUV;
+let tex;
+let newImage1, newImage2;  // Zmienna do nowych grafik
 
 function preload() {
-    mario = loadModel('model/model.obj', true); // Wczytanie modelu 3D
+  mario = loadModel('model/model.obj', true); // Załaduj model
+  marioUV = loadImage('img/steve.png');       // Załaduj teksturę
+  newImage1 = loadImage('img/steve.png'); // Załaduj pierwszą nową grafikę
+  newImage2 = loadImage('img/jaskiniowiec_skin.png'); // Załaduj drugą nową grafikę
 }
 
 function setup() {
-    // Tworzenie canvasu dla modelu 3D
-    const modelCanvas = createCanvas(400, 400, WEBGL);
-    modelCanvas.parent('model-canvas');
+  let canvas = createCanvas(400, 400, WEBGL);
 
-    // Tworzenie pustej tekstury za pomocą createImage
-    combinedTexture = createImage(64, 64); // Używamy createImage, aby móc modyfikować piksele
+  // Utwórz teksturę dla obrazu
+  tex = createTexture(marioUV);  // Tworzenie tekstury z obrazka 'steve.png'
 
-    // Dynamiczne aktualizowanie tekstury
-    setInterval(() => {
-        const sourceCanvas = document.getElementById('canvas');
-        const ctx = sourceCanvas.getContext('2d');
-        const imageData = ctx.getImageData(0, 0, sourceCanvas.width, sourceCanvas.height);
-
-        combinedTexture.loadPixels(); // Wczytanie pikseli w combinedTexture
-        for (let i = 0; i < imageData.data.length; i++) {
-            combinedTexture.pixels[i] = imageData.data[i];
-        }
-        combinedTexture.updatePixels(); // Zaktualizowanie pikseli w combinedTexture
-    }, 100);
+  // Ustawienie interpolacji dla ostrych tekstur
+  tex.setInterpolation(NEAREST, NEAREST);
 }
 
 function draw() {
-    background(220);
-    orbitControl();
+  background(220);
+  orbitControl(); // Dodano ręczną kontrolę kamery (możesz używać myszy, jeśli chcesz)
 
-    // Obracanie modelu dla właściwej orientacji
-    rotateX(0);
-    rotateZ(PI);
+  // Obróć model w celu poprawy orientacji
+  rotateX(PI);  // Obraca model do właściwej orientacji (jeśli jest do góry nogami)
+  noStroke();
 
-    // Nakładanie tekstury na model 3D
-    if (combinedTexture) {
-        texture(combinedTexture); // Nakładanie tekstury na model
-    } else {
-        noTexture();
-    }
-    model(mario);
+  // Renderowanie modelu z teksturą
+  // Możesz nałożyć różne tekstury w zależności od warunków lub wejścia
+  texture(newImage1); // Nakłada pierwszą nową teksturę
+  model(mario);       // Wyświetl model
+
+  // Jeśli chcesz zmienić teksturę na drugą grafikę, użyj np. conditionals lub zmiennych
+  // W tym przypadku na przykład zmieniając teksturę na 'newImage2' po jakimś czasie
+  texture(newImage2); // Nakłada drugą nową teksturę
+  model(mario);       // Wyświetl model ponownie z drugą teksturą
 }
 
 
