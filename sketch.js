77b4e45c13062cf66ae4f18bf7d8e4e1390c1991
szczypikrul <42851381+szczypikrul@users.noreply.github.com>
2025-05-16 02:29:27 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-/*
+
 let mario;
 let marioUV;
 let tex;
@@ -29,36 +29,7 @@ function draw() {
   texture(marioUV); // Nałóż teksturę na model
   model(mario);     // Wyświetl model
 }
-*/
-let mario;
-let dynamicCanvas;
-let dynamicTexture;
 
-function preload() {
-  mario = loadModel('model/model.obj', true); // Ładuj model
-}
 
-function setup() {
-  createCanvas(400, 400, WEBGL);
-  noStroke();
-
-  // Pobierz referencję do HTML canvas (z łączenia grafik)
-  dynamicCanvas = document.getElementById('canvas');
-
-  // Ustaw początkową teksturę jako grafikę z tego canvasu
-  dynamicTexture = createGraphics(128, 128);
-}
-
-function draw() {
-  background(220);
-  orbitControl();
-  rotateX(PI);
-
-  // Skopiuj zawartość HTML canvas do p5.Graphics
-  dynamicTexture.drawingContext.drawImage(dynamicCanvas, 0, 0);
-
-  texture(dynamicTexture);
-  model(mario);
-}
 
 
